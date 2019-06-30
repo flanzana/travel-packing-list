46ca45769184fae96a8c931ad34723b9a6cd7581
Zana Flander <zana.flander@kiwi.com>
2019-06-30 21:18:42 +0200

    Add: disable/enable checkbox

--- src/components/TravelItem.js
+++ src/components/TravelItem.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
+import { defaultTokens } from "@kiwicom/orbit-design-tokens";
+import Stack from "@kiwicom/orbit-components/lib/Stack";
 import Checkbox from "@kiwicom/orbit-components/lib/Checkbox";
+import ButtonLink from "@kiwicom/orbit-components/lib/ButtonLink";
+import Close from "@kiwicom/orbit-components/lib/icons/Close";
+import Plus from "@kiwicom/orbit-components/lib/icons/Plus";
+
+const { heightCheckbox, widthCheckbox } = defaultTokens;
+
+const StyledButtonLink = styled(ButtonLink)`
+  height: ${heightCheckbox};
+  width: ${widthCheckbox};
+`;
 
 function TravelItem({ item }) {
   const [ active, setActive ] = useState(false);
+  const [ disabled, setDisabled ] = useState(false);
+
+  const handleDisabled = () => {
+    setDisabled(!disabled);
+    setActive(false);
+  };
 
   return (
-    <Checkbox
-      label={item}
-      name={item}
-      value={item}
-      checked={active}
-      onChange={() => setActive(!active)}
-    />
+    <Stack direction="row" align="start" justify="between" spacing="natural">
+      <Checkbox
+        label={item}
+        name={item}
+        value={item}
+        checked={!disabled && active}
+        onChange={() => setActive(!active)}
+        disabled={disabled}
+      />
+      <StyledButtonLink
+        type="secondary"
+        size="small"
+        iconLeft={disabled ? <Plus color="secondary" /> : <Close color="critical" />}
+        title={disabled ? `Enable ${item}` : `Disable ${item}`}
+        circled
+        transparent
+        onClick={() => handleDisabled()}
+      />
+    </Stack>
   );
 }
 
