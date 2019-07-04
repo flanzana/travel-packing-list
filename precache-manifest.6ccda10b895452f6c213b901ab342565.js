a9b161fbaea99c599e5b5c09c02bb343e1724b94
Zana Flander <zana.flander@kiwi.com>
2019-07-04 19:06:33 +0200

    Updates

---
+++ precache-manifest.6ccda10b895452f6c213b901ab342565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b8fce0faa70a9b0992dcc02b4bcebe",
+    "url": "/travel-packing-list/index.html"
+  },
+  {
+    "revision": "5b4d01d1c80b13fcba54",
+    "url": "/travel-packing-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6ed74c5c6d2b957e263d",
+    "url": "/travel-packing-list/static/js/2.fe0ee8a2.chunk.js"
+  },
+  {
+    "revision": "5b4d01d1c80b13fcba54",
+    "url": "/travel-packing-list/static/js/main.7b966b73.chunk.js"
+  },
+  {
+    "revision": "af56610e9bb84f2be738",
+    "url": "/travel-packing-list/static/js/runtime~main.0702185b.js"
+  }
+]);
