96ef9c0c02fbbdbffee0991b7ff2aa25f6fd5a83
Zana Flander <zana.flander@kiwi.com>
2020-05-31 15:32:23 +0200

    Style: update footer background and add 2020 year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from "styled-components";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { defaultTokens } from "@kiwicom/orbit-design-tokens";
 import Stack from "@kiwicom/orbit-components/lib/Stack";
 import Text from "@kiwicom/orbit-components/lib/Text";
 import TextLink from "@kiwicom/orbit-components/lib/TextLink";
 
 const {
-  paletteInkLightActive,
+  borderColorCard,
   widthBreakpointLargeMobile,
   spaceSmall,
   spaceMedium,
@@ -15,25 +15,22 @@ const {
 } = defaultTokens;
 
 const FooterWrapper = styled.div`
-  background-color: ${paletteInkLightActive};
-  padding: ${spaceSmall} ${spaceXXLarge};
-  
-  a {
-    color: white !important;
-  };
+  background-color: ${borderColorCard};
+  padding: 0 ${spaceXXLarge} ${spaceSmall};
   
   @media screen and (min-width: ${widthBreakpointLargeMobile}px) {
-    padding: ${spaceMedium};
+    padding: 0 ${spaceMedium} ${spaceMedium};
   };
 `;
 
 function Footer() {
+  const { t } = useTranslation("translations");
   return (
     <FooterWrapper>
       <Stack direction="column" align="center" justify="center" dataTest="Footer">
-        <Text size="small" type="white" align="center">
-          <Trans i18nKey="footer.text">
-            Copyright © 2019 <TextLink href="https://flanzana.github.io/" external>Žana Flander</TextLink> and <TextLink href="https://orbit.kiwi" external>Orbit</TextLink>
+        <Text size="small" align="center">
+          <Trans t={t} i18nKey="footer.text">
+            © 2019-2020 <TextLink href="https://flanzana.github.io/" external>Žana Flander</TextLink> and <TextLink href="https://orbit.kiwi" external>Orbit</TextLink>
           </Trans>
         </Text>
       </Stack>
