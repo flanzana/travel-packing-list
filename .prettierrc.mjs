bdef81ae36eb5ae2e61208afeb577ef15cf5b002
zana.flander <zana.flander@kiwi.com>
2024-06-28 16:15:48 +0200

    chore(deps): update all dependencies except eslint

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,4 +1,4 @@
-{
+export default {
   "printWidth": 100,
   "trailingComma": "all",
   "semi": false,
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom/extend-expect"
+import "@testing-library/jest-dom/vitest"
 import { configure } from "@testing-library/react"
 
 configure({ testIdAttribute: "data-test" })
